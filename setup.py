dd56ff703d00baa23663473adcda3ce8376f9a34
kevin <thekevinyu@gmail.com>
2018-07-17 12:52:18 -0700

    Add setup.py so it can be installed with pip

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import os
+
+long_description = open("README.md").read()
+
+setup(
+    name = "intanutil",
+    version = '0.0',
+    packages = ['intanutil'],
+    install_requires=[],
+    extras_require={},
+    author = "",
+    author_email = "",
+    description = "Load Intan RHD file",
+    long_description = long_description,
+    classifiers=[]
+)
