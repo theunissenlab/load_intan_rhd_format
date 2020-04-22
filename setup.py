fab8bb996f4f8c357e8e58621529e0441c5753d7
ftheunissen <theunissen@berkeley.edu>
2020-04-22 15:39:25 -0700

    Update setup.py
    Bumped the version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = open("README.md").read()
 
 setup(
     name = "intanutil",
-    version = '0.0',
+    version = '0.1',
     packages = ['intanutil'],
     install_requires=[],
     extras_require={},
